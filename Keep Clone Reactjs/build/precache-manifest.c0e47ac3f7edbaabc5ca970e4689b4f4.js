9d9b4c26913d534901d4f710d25621fb873ca26c
moinuddinvaidya786 <102137468+moinuddinvaidya786@users.noreply.github.com>
2022-04-11 18:48:14 +0530

    Add files via upload

---
+++ Keep Clone Reactjs/build/precache-manifest.c0e47ac3f7edbaabc5ca970e4689b4f4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5f67f484d41cfc18d44",
+    "url": "/static/js/main.d5f67f48.chunk.js"
+  },
+  {
+    "revision": "22c9e3f91e55516ce23d",
+    "url": "/static/js/1.22c9e3f9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1decf92dc1562cbdf763ed9b738287ef",
+    "url": "/index.html"
+  }
+];
---
+++ Keep Clone Reactjs/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+var d = new Date();
+var currentYear = d.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <span>Created By Moinuddin Vaidya</span>
+      <p>Copyright &copy; {currentYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
